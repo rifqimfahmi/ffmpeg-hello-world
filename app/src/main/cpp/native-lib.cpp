25a3a128e1ece5d35e977af8b7ea2098c0b7e274
rifqimfahmi <rifqi416@gmail.com>
2020-09-25 23:05:31 +0700

    link ffmpeg lib to jni

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_rifqimfahmi_helloworldffmpeeg_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++17";
     return env->NewStringUTF(hello.c_str());
 }
